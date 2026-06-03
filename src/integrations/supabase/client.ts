c95dc5fb2e20553bc8e413be82a64e06dfc0badb
Danilo <new@192.168.2.10>
2026-06-03 20:26:13 -0300

    chore: migrate frontend to official supabase project

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,9 +3,9 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types";
 
-const SUPABASE_URL = "https://gseindfuqkumktbobyny.supabase.co";
+const SUPABASE_URL = "https://qfzgjgkdbblysdslensu.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzZWluZGZ1cWt1bWt0Ym9ieW55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxMjQ5NTQsImV4cCI6MjA5NTcwMDk1NH0.q02UR6_wl9dpPa2FL4q-vVgBzhrrrbt0iJ2HcdI1zlI";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmemdqZ2tkYmJseXNkc2xlbnN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0Nzg5NDEsImV4cCI6MjA5NjA1NDk0MX0.hFxKypGu_6fSg-UcljCMu6gST1dFi1Z2ttDxLp_Lans";
 
 const CLIENT_OPTIONS = {
   auth: {
